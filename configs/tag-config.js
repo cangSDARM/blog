b8d36b9462e41ae0bb9c08cbff85d5295b68f479
cangSDARM <648384410li@gmail.com>
2021-07-24 22:00:55 +0800

    fix config

--- configs/tag-config.js
+++ configs/tag-config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const { kebabCase } = require("lodash");
 
-const tagTemplate = path.resolve("src/templates/tags.js"); //resolve从项目目录开始的……有点奇怪
+const tagTemplate = path.resolve("src/templates/tags.ts"); //resolve从项目目录开始的……有点奇怪
 
 exports.tagComponentInject = function (tags, createPage) {
   tags.forEach((tag) => {
