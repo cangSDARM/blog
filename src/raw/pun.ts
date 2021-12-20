4a10b40e52aea488ec2a7c5449bef0e6d236a119
Allen\FuckGit <648384410li@gmail.com>
2021-12-20 15:52:47 +0800

    update gh-pages.yml

--- src/raw/pun.ts
+++ src/raw/pun.ts
@@ -1,6 +1,7 @@
 const puns = [
   "The world is Complex, so it is not Real.",
   "We use TypeScript as any as possible.",
+  "Knowledge is power, France is Bacon(Fancis Bacon).",
   "Across the Great Wall we can reach every corner in the world.",
   "月薪3千的骂美国，月薪3万的学美国，月薪30万的自己移民去美国，月薪300万的，自己好好在国内，全家全部在美国。",
   "红伞伞，白杆杆，吃完一起躺板板，躺板板睡棺棺，然后一起埋山山，埋山山哭喊喊，亲朋都来吃饭饭，吃饭饭有伞伞，全村一起躺板板。一起躺板板，无人埋山山，全都一起风干干，来年长出红伞伞。",
